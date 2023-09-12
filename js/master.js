8f976d8b2912fa86de43ff180d84278c24bb9484
dnl243 <dnl243@hotmail.com.ar>
2023-09-12 00:16:10 -0300

    playingWhitTheDOM

--- js/master.js
+++ js/master.js
@@ -1,13 +1,27 @@
-let mac5 = document.querySelector("#mach5");
+// Captura de elementos
+let car = document.querySelector("#car");
 let body = document.querySelector("body");
-
+let btnMeteoro = document.querySelector("#meteoro");
+let btnPolicia = document.querySelector("#policia");
+let btnAmbulancia = document.querySelector("#ambulancia")
+// Variables de posición
 let mLeft = 0;
 let mTop = 0;
 let speed = 10;
-
-mach5.innerHTML =
-  '<img src="./img/mach5-right.jpg" alt="mach5" style="width:110px;"></img>';
-
+// Insertar incógnita
+car.innerHTML =
+  '<img src="../img/en-espera.jpg" alt="en-espera"></img>';
+// Selección  de vehículos
+btnMeteoro.addEventListener("click", () =>
+car.innerHTML =
+  '<img src="../img/mach5-right.jpg" alt="meteoro"></img>');
+btnPolicia.addEventListener("click", () =>
+car.innerHTML =
+'<img src="../img/policia-right.jpg" alt="policia"></img>');
+btnAmbulancia.addEventListener("click", () =>
+car.innerHTML =
+'<img src="../img/ambulancia-right.jpg" alt="ambulancia"></img>');
+// Movimiento de vehículo
 body.addEventListener("keydown", function (evento) {
   console.log(evento);
   if (evento.key === "ArrowRight") {
@@ -22,30 +36,38 @@ body.addEventListener("keydown", function (evento) {
 });
 
 function moveRight() {
-  mach5.innerHTML =
-    '<img src="./img/mach5-right.jpg" alt="mach5" style="width:110px;"></img>';
+  car.classList.remove("left");
+  car.classList.remove("up");
+  car.classList.remove("down");
+  car.classList.add("right");
   mLeft += speed;
   console.log(mLeft);
-  mach5.style.marginLeft = mLeft + "px";
+  car.style.marginLeft = mLeft + "px";
 }
 function moveLeft() {
-  mach5.innerHTML =
-    '<img src="./img/mach5-left.jpg" alt="mach5" style="width:110px;"></img>';
+  car.classList.remove("right");
+  car.classList.remove("up");
+  car.classList.remove("down");
+  car.classList.add("left");
   mLeft -= speed;
   console.log(mLeft);
-  mach5.style.marginLeft = mLeft + "px";
+  car.style.marginLeft = mLeft + "px";
 }
 function moveUp() {
-  mach5.innerHTML =
-    '<img src="./img/mach5-up.jpg" alt="mach5" style="height:110px;"></img>';
+  car.classList.remove("left");
+  car.classList.remove("right");
+  car.classList.remove("down");
+  car.classList.add("up");
   mTop -= speed;
   console.log(mTop);
-  mach5.style.marginTop = mTop + "px";
+  car.style.marginTop = mTop + "px";
 }
 function moveDown() {
-  mach5.innerHTML =
-    '<img src="./img/mach5-down.jpg" alt="mach5" style="height:110px;"></img>';
+  car.classList.remove("left");
+  car.classList.remove("up");
+  car.classList.remove("right");
+  car.classList.add("down");
   mTop += speed;
   console.log(mTop);
-  mach5.style.marginTop = mTop + "px";
+  car.style.marginTop = mTop + "px";
 }
