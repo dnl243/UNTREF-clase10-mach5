7371a9e8e52799b16c9542f7d4a54a28f4915b4e
dnl243 <dnl243@hotmail.com.ar>
2023-09-18 22:59:02 -0300

    addLocalStorage

--- js/master.js
+++ js/master.js
@@ -9,28 +9,38 @@ Swal.fire({
   }
 })
 // Captura de elementos
-let car = document.querySelector("#car");
-let body = document.querySelector("body");
-let btnMeteoro = document.querySelector("#meteoro");
-let btnPolicia = document.querySelector("#policia");
-let btnAmbulancia = document.querySelector("#ambulancia")
+const car = document.querySelector("#car");
+const body = document.querySelector("body");
+const btnMeteoro = document.querySelector("#meteoro");
+const btnPolicia = document.querySelector("#policia");
+const btnAmbulancia = document.querySelector("#ambulancia");
+const selectCar = localStorage.getItem('car');
 // Variables de posición
 let mLeft = 0;
 let mTop = 0;
 let speed = 1;
 // Elemento en espera
 car.innerHTML =
-  '<img src="./img/en-espera.jpg" alt="en-espera"></img>';
+'<img src="./img/en-espera.jpg" alt="en-espera"></img>';
 // Selección  de vehículos
-btnMeteoro.addEventListener("click", () =>
-car.innerHTML =
-  '<img src="./img/mach5.jpg" alt="meteoro"></img>');
-btnPolicia.addEventListener("click", () =>
-car.innerHTML =
-'<img src="./img/policia.jpg" alt="policia"></img>');
-btnAmbulancia.addEventListener("click", () =>
-car.innerHTML =
-'<img src="./img/ambulancia.jpg" alt="ambulancia"></img>');
+btnMeteoro.addEventListener("click", function(){
+  car.innerHTML ='<img src="./img/mach5.jpg" alt="meteoro"></img>';
+  localStorage.setItem('car','meteoro')});
+btnPolicia.addEventListener("click", function(){
+  car.innerHTML ='<img src="./img/policia.jpg" alt="policia"></img>';
+  localStorage.setItem('car','policia')});
+btnAmbulancia.addEventListener("click", function(){
+  car.innerHTML ='<img src="./img/ambulancia.jpg" alt="ambulancia"></img>';
+  localStorage.setItem('car','ambulancia')});
+
+if(selectCar === 'meteoro'){
+  car.innerHTML ='<img src="./img/mach5.jpg" alt="meteoro"></img>';
+}else if (selectCar === 'policia'){
+  car.innerHTML ='<img src="./img/policia.jpg" alt="policia"></img>';
+}else if (selectCar === 'ambulancia'){
+  car.innerHTML ='<img src="./img/ambulancia.jpg" alt="ambulancia"></img>'; 
+};
+
 // Movimiento de vehículo con límite de pantalla
 body.addEventListener("keydown", function (evento) {
   if (mLeft < 99 && evento.key === "d") {
@@ -66,12 +76,12 @@ function gameOver() {
   car.classList.remove("down");
   car.classList.add("right");
   anotherChance.fire({
-      title: 'Te lo dije!!',
-      text: "¿Queres jugar denuevo?",
-      imageUrl: 'https://media.tenor.com/yp62lkWc5I4AAAAM/muttley-laughing.gif',
-      imageWidth: 200,
-      imageHeight: 175,
-      imageAlt: 'Patan',
+    title: 'Te lo dije!!',
+    text: "¿Queres jugar denuevo?",
+    imageUrl: 'https://media.tenor.com/yp62lkWc5I4AAAAM/muttley-laughing.gif',
+    imageWidth: 200,
+    imageHeight: 175,
+    imageAlt: 'Patan',
       showCancelButton: true,
       confirmButtonText: 'SI!',
       cancelButtonText: 'No',
@@ -90,20 +100,20 @@ function gameOver() {
         })
       } else if (
         result.dismiss === Swal.DismissReason.cancel
-      ) {
-        anotherChance.fire({
-          title: 'Saludos!',
-          text: 'Mejor suerte la próxima!',
-          imageUrl: 'https://i.gifer.com/A0cF.gif',
-          imageWidth: 400,
-          imageHeight: 200,
-          imageAlt: 'ambulancia',
-          confirmButtonColor: '#3085d6',
-          confirmButtonText: '<i class="bi bi-door-open"></i>'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.close();
-          }
+        ) {
+          anotherChance.fire({
+            title: 'Saludos!',
+            text: 'Mejor suerte la próxima!',
+            imageUrl: 'https://i.gifer.com/A0cF.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'ambulancia',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: '<i class="bi bi-door-open"></i>'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.close();
+            }
         })
       }
     });
@@ -145,3 +155,48 @@ function moveDown() {
   console.log(mTop);
   car.style.marginTop = mTop + "%";
 }
+
+// Modo claro/oscuro
+// Captura de elementos
+const btnLightMode = document.querySelector("#lightMode");
+const btnDarkMode = document.querySelector("#darkMode");
+const navbar = document.querySelector("#navbar");
+const changeMode = localStorage.getItem('mode');
+// Modo claro por defecto
+btnLightMode.addEventListener('click', function(){
+  navbar.classList.remove('bg-dark', 'text-light');
+  localStorage.removeItem('mode', 'black');
+  navbar.classList.add('bg-body-tertiary');
+  localStorage.setItem('mode', 'white');
+});
+
+// Modo oscuro
+btnDarkMode.addEventListener('click', function(){
+  navbar.classList.remove('bg-body-tertiary');
+  localStorage.removeItem('mode', 'white');
+  navbar.classList.add('bg-dark', 'text-light');
+  localStorage.setItem('mode', 'black');
+});
+
+if (changeMode === 'white'){
+  navbar.classList.remove('bg-dark', 'text-light');
+  navbar.classList.add('bg-body-tertiary');
+} else if (changeMode === 'black'){
+  navbar.classList.remove('bg-body-tertiary');
+  navbar.classList.add('bg-dark', 'text-light');
+}
+ 
+
+// pablo mencionó en la clase anterior que se pierde el autito cuando reiniciamos la página, ya lo resolvimos
+
+
+
+
+
+
+
+
+
+
+
+
